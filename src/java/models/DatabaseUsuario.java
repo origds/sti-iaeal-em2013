c6595f98f9fa30b1e0159619e542a81f3b29e175
= <=>
2013-01-29 18:45:58 -0430

    Listo el log de busqueda

--- src/java/models/DatabaseUsuario.java
+++ src/java/models/DatabaseUsuario.java
@@ -167,7 +167,7 @@ public class DatabaseUsuario {
       System.out.println(sqlquery);
       Statement st = database.getConnection().createStatement();
       Integer i = st.executeUpdate(sqlquery);
-      System.out.println("INSERTE EN EL LOG: " + i);
+      System.out.println("INSERTE EN EL LOG: " + i); //bien
       return i > 0;
     } catch (SQLException ex) {
       ex.printStackTrace();
