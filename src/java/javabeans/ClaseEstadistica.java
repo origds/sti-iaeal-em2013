a0a85c39d5ea058d6e6d759e5323c21d5d0e79e3
Oriana Gomez <origds@gmail.com>
2013-02-08 16:33:39 -0430

    estadisticas con periodo

--- src/java/javabeans/ClaseEstadistica.java
+++ src/java/javabeans/ClaseEstadistica.java
@@ -15,14 +15,18 @@ public class ClaseEstadistica extends ActionForm {
    private String codigo1;
    private String codigo2;
    private String codigo3;
+   private int periodoIni;
+   private int periodoFin;
 
     public ClaseEstadistica() {
     }
 
-    public ClaseEstadistica(String codigo1, String codigo2, String codigo3) {
+    public ClaseEstadistica(String codigo1, String codigo2, String codigo3, int periodoIni, int periodoFin) {
         this.codigo1 = codigo1;
         this.codigo2 = codigo2;
         this.codigo3 = codigo3;
+        this.periodoIni = periodoIni;
+        this.periodoFin = periodoFin;
     }
 
     public String getCodigo1() {
@@ -49,5 +53,20 @@ public class ClaseEstadistica extends ActionForm {
         this.codigo3 = codigo3;
     }
 
-   
+  public int getPeriodoIni() {
+    return periodoIni;
+  }
+
+  public void setPeriodoIni(int periodoIni) {
+    this.periodoIni = periodoIni;
+  }
+
+  public int getPeriodoFin() {
+    return periodoFin;
+  }
+
+  public void setPeriodoFin(int periodoFin) {
+    this.periodoFin = periodoFin;
+  }
+      
 }
