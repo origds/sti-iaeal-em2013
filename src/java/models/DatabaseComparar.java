ab6d1ddcd1866628949cb00d791765e085e7809d
Oriana Gomez <origds@gmail.com>
2013-03-10 15:20:32 -0430

    Esta hecho el DatabaseComparar falta el GestionComparar

--- src/java/models/DatabaseComparar.java
+++ src/java/models/DatabaseComparar.java
@@ -9,11 +9,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import javabeans.Buscable;
-import javabeans.Tratado;
-import javabeans.est;
 import javabeans.Comparar;
+import javabeans.Tratado;
 
 /**
  *
@@ -24,6 +21,67 @@ public class DatabaseComparar {
   private Database database;
   static private Connection conexion;
 
-  protected DatabaseComparar() {
+  public DatabaseComparar(String driver, String databaseUrl) {
+    this.database = Database.getInstance(driver, databaseUrl);
   }
+  
+  public Comparar compararTratados(Comparar c) {
+    String sqlcomparar= "";
+    
+    try {
+      if ((c.getPeriodoIni1() == 0) & (c.getPeriodoFin1() == 0)) {
+        if ((c.getPeriodoIni2() == 0) & (c.getPeriodoFin2() == 0)) {
+          throw new Exception("Debe ingresar un periodo valido");
+        }
+      } 
+      
+      if ((c.getPeriodoIni1()!=0) && (c.getPeriodoFin1()!=0)) {
+        if (c.getPeriodoIni1() > c.getPeriodoFin1()) {
+          throw new Exception("El inicio del periodo 1 debe ser mayor al final");
+        }
+        
+        sqlcomparar = "SELECT COUNT (*) FROM \"STI\".tratado WHERE (" + c.getPeriodoIni1() 
+                      + " <= (extract(year from firmaFecha)) " 
+                      + "AND (extract(year from firmaFecha)) <= " 
+                      + c.getPeriodoFin1() + ")";
+        
+        System.out.println(sqlcomparar);
+        Statement stmt = conexion.createStatement();
+        ResultSet rs = stmt.executeQuery(sqlcomparar);
+        while (rs.next()) {
+          c.setCantidad1("count");
+        }
+        System.out.println("CANTIDAAAD 1");
+        System.out.println(c.getCantidad1());
+      }
+      
+      if ((c.getPeriodoIni2()!=0) && (c.getPeriodoFin2()!=0)) {
+        if (c.getPeriodoIni2() > c.getPeriodoFin2()) {
+          throw new Exception("El inicio del periodo 2 debe ser mayor al final");
+        }
+        
+        sqlcomparar = "SELECT COUNT (*) FROM \"STI\".tratado WHERE (" + c.getPeriodoIni2() 
+                      + " <= (extract(year from firmaFecha)) " 
+                      + "AND (extract(year from firmaFecha)) <= " 
+                      + c.getPeriodoFin2() + ")";
+        
+        System.out.println(sqlcomparar);
+        Statement stmt = conexion.createStatement();
+        ResultSet rs = stmt.executeQuery(sqlcomparar);
+        while (rs.next()) {
+          c.setCantidad2("count");
+        }
+        System.out.println("CANTIDAAAD 2");
+        System.out.println(c.getCantidad2());
+      }
+        
+    } catch (SQLException ex) {
+      System.out.println("EXCEPCION");
+      ex.printStackTrace();
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
+    
+    return c; 
+  }    
 }
